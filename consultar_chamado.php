fb7426d43b1ff1716d691ba37fdfe8b733570ef0
Kaike Rocha <kaikerochadev@gmail.com>
2024-01-06 15:16:36 -0300

    Changing the project title to Portuguese

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -16,7 +16,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>App Help Desk</title>
+    <title>Suporte técnico do aplicativo</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -34,7 +34,7 @@
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
         <img src="./imgs/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
+        Suporte técnico do aplicativo
       </a>
       <ul class="navbar-nav">
         <li class="nav-item">
